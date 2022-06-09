69f7074ad05b8ea07c84628e36b6c7c1948bd194
Alexr16 <j.alex-1996@hotmail.com>
2022-06-09 18:10:10 -0500

    Clean comments

--- my_list.rb
+++ my_list.rb
@@ -12,19 +12,3 @@ class MyList
     end
   end
 end
-
-# for make a test undo comments
-
-# New Class my_list
-# my_list = MyList.new([1, 2, 3, 4])
-
-# Test _any method
-# puts my_list._any { |e| e == 2 }
-# p my_list._any { |e| e == 5 }
-
-# Test _all method
-# p my_list._all { |e| e < 5 }
-# p my_list._all { |e| e > 5 }
-
-# Test _filter method
-# p my_list._filter { |e| e.even? }
