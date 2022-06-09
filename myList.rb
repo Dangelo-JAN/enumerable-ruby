8efed51b95e22627050ff80cad20836dc90c249c
Alexr16 <j.alex-1996@hotmail.com>
2022-06-09 16:46:01 -0500

    Implement _any method

--- myList.rb
+++ myList.rb
@@ -1,9 +1,16 @@
+require_relative ('MyEnumerable.rb')
+
 class MyList
+    include MyEnumerable
     def initialize(list)
         @list = list
     end
 
     def each
-        yield ( @list ) if block_given?
+      @list.each { |x| yield(x) } if block_given?
     end
 end
+
+my_list = MyList.new([1,2,3,4,5])
+p my_list._any { |e| e < 5 }
+p my_list._any { |e| e > 5 }
